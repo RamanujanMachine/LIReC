073dfbd2e8dd5e96660660a65c5787a4415a1c16
eyalk <eyal.kalman@gmail.com>
2023-03-09 17:24:24 +0200

    Change setup.py file to LIRec and to have only relevent requirements. Added __init__ file to LIReC directory

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="ramanujan",
+    name="LIReC",
     version="0.0.1",
     # due to Amazon EC2 issues, we probably can't go beyond Python3.9
     # (Python3.10.8 seems to have issues with pip). As such, I don't care if
@@ -12,32 +12,13 @@ setup(
     # https://github.com/numba/numba/issues/8304 ; https://github.com/google/or-tools/issues/3515
     # Because of this, upgrading to Python3.11 will also require these packages to update.
     python_requires=">=3.8.10, <3.9",
-    description="Ramanujan Machine",
-    packages=['ramanujan'],
+    description="Library of Integer RElations and Constants",
+    packages=['lib'],
     install_requires=[
-        'cycler>=0.10.0',
-        'gmpy2>=2.1.5',
-        'kiwisolver>=1.1.0',
-        'matplotlib>=3.3.3',
-        'mpmath>=1.2.1',
-        'numpy>=1.19.5',
-        'ordered-set>=3.1.1',
-        'ortools>=7.4.7247',
-        'pandas>=1.0.1',
-        'protobuf>=3.11.3',
-        'psutil>=5.5.1',
-        'psycopg2>=2.8.6',
-        'pybloom-live',
-        'PyLaTeX>=1.3.1',
-        'pyparsing>=2.4.6',
-        'pytest>=6.2.4',
-        'python-dateutil>=2.8.1',
-        'pytz>=2019.3',
-        'scipy>=1.6.0',
-        'simplejson>=3.16.0',
-        'six>=1.14.0',
-        'sqlacodegen>=2.3.0',
+        'sqlalchemy>=2.0.5',
         'sympy>=1.5.1',
-        'xlrd>=2.0.1',
+        'psycopg2>=2.8.6',
+        'mpmath>=1.2.1',
+        'gmpy2>=2.1.5',
     ]
 )

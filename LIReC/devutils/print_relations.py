d42a2b608a8012be1aaa4d60230fdb4e42f4d501
itaybthl <30323792+itaybthl@users.noreply.github.com>
2023-11-14 18:43:57 +0200

    added sources and vague relations to print_relations
    built-in explanations to some relations! next step is transitive explanations

--- LIReC/devutils/print_relations.py
+++ LIReC/devutils/print_relations.py
@@ -11,13 +11,15 @@ def main():
     print(f'printing relations one at a time in descending order of precision{"" if keep_going else ", press enter to print the next"}')
     n = Symbol('n')
     rels = db.session.query(Relation).order_by(Relation.precision.desc()).all()
+    rels_vague = [r for r in rels if r.relation_type=='VAGUE']
+    rels = [r for r in rels if r.relation_type!='VAGUE']
     nameds = db.session.query(NamedConstant).all()
     pcfs = db.session.query(PcfCanonicalConstant).all()
     for rel in rels:
         exponents = get_exponents(*rel.details[:2], len(rel.constants))
         monoms = [reduce(add, (f'*c{i}**{exp[i]}' for i in range(len(rel.constants))), f'{rel.details[2:][j]}') for j, exp in enumerate(exponents)]
         poly = Poly(reduce(add, ['+'+monom for monom in monoms], ''))
-        toprint = f'poly: {poly.expr}, precision: {rel.precision}' + ', consts: {\r\n'
+        toprint = f'\r\npoly: {poly.expr}, precision: {rel.precision}' + ', consts: {\r\n'
         for const in rel.constants:
             named = [n for n in nameds if n.const_id == const.const_id]
             if named:
@@ -26,19 +28,19 @@ def main():
                 pcf = [p for p in pcfs if p.const_id == const.const_id]
                 if pcf:
                     pcf = pcf[0]
-                    if pcf.original_a and pcf.original_b:
-                        b_factors = Poly(pcf.original_b, n).factor_list()
-                        if len(b_factors[1]) > 1: # usually b_n tends to have only nonpositive integer roots, so try to extract them!
-                            b_string = reduce(add, [((f'*({root.expr})' if root.expr != n else '*n') + (f'**{mult}' if mult > 1 else '')) for root, mult in b_factors[1]], f'{b_factors[0]}')
-                            toprint += f'    a: {Poly(pcf.original_a, n).expr}, b: {b_string}'
-                        else:
-                            toprint += f'    a: {Poly(pcf.original_a, n).expr}, b: {Poly(pcf.original_b, n).expr}'
-                    else:
-                        toprint += f'    P: {Poly(pcf.P, n).expr}, Q: {Poly(pcf.Q, n).expr}'
+                    toprint += f'    P: {Poly(pcf.P, n).expr}, Q: {Poly(pcf.Q, n).expr}'
                 else:
                     print(f'constant with uuid {const.const_id} has no known extension')
-            toprint += f', precision: {const.precision}, value: {str(const.value)[:50]}...' + '\r\n'
+            toprint += f', precision: {const.precision}, value: {str(const.value)[:50]}...'
+            if const.source_notes:
+                toprint += f', source: {const.source_ref.alias} ({const.source_notes})'
+            toprint += '\r\n'
+        
         print(toprint + '} (uuids: ' + f'{[str(c.const_id) for c in rel.constants]})')
+        
+        ids = {c.const_id for c in rel.constants}
+        if any(v for v in rels_vague if ids == {c.const_id for c in v.constants}):
+            print('This is exactly predicted by a vague relation!')
         if not keep_going:
             input()
 

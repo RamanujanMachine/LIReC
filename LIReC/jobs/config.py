73d40c80f8b6a4f7fba0ac4c50466511a396fd40
itaybthl <30323792+itaybthl@users.noreply.github.com>
2024-06-22 00:43:15 +0300

    multiprocessing final fix
    so far i found out the problem might be with running the same job with different configurations... will fix later, but at least i know how to replicate the weird KeyError and how to fix it

--- LIReC/jobs/config.py
+++ LIReC/jobs/config.py
@@ -2,25 +2,14 @@ configuration = {
     'pool_size': 8,
     'jobs_to_run': [
         ('poly_pslq_v1', {
-            'args': { 'degree': 5, 'order': 2, 'bulk': 1000, 'filters': {
+            'args': { 'degree': 2, 'order': 1, 'bulk': 1000, 'filters': {
                 'global': { 'min_precision': 50 },
-                'PcfCanonical': { 'count': 2, 'balanced_only': False },
-                'Named': { 'count': 1, 'addons': ['pi*e'] }
-                }
-            },
-            'run_async': True,
-            'async_cores': 4
-        }),
-        ('poly_pslq_v1', {
-            'args': { 'degree': 2, 'order': 2, 'bulk': 1000, 'filters': {
-                'global': { 'min_precision': 30 },
-                'PcfCanonical': { 'count': 2, 'balanced_only': False, 'addons': ['1/PCF[1,n]', '1/(PCF[2+2n,1-2n]-1)'] },
+                'PcfCanonical': { 'count': 1, 'balanced_only': False },
                 'Named': { 'count': 2 }
                 }
             },
-            'run_async': True,
-            'async_cores': 4
-        })#,
+            'run_async': True
+        }),
         #'poly_pslq_v2', {
         #    'args': {
         #        'degree': 2, 'order': 1,

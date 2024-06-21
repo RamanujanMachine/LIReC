096eef8bd4d73733059d424e2c5ce66c6a1946d3
itaybthl <30323792+itaybthl@users.noreply.github.com>
2024-06-21 17:17:47 +0300

    logger fix 5/?

--- LIReC/lib/logger.py
+++ LIReC/lib/logger.py
@@ -7,7 +7,7 @@ def configure_logger(name):
         if not os.path.exists('logs'):
             os.mkdir('logs')
         for p in sys.path:
-            p2 = os.path.join(p, 'logging.config')
+            p2 = os.path.join(p, 'LIReC/logging.config')
             if os.path.exists(p2):
                 logging.config.fileConfig(p2, defaults={'log_filename': name})
                 logger = logging.getLogger('job_logger')
